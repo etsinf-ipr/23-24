04d94dcca3da0a648a8ab1c1525dd4c071ed114a
MREBOLLO <mrebollo@upvnet.upv.es@ldsic-vdi13.upvnet.upv.es>
2023-09-18 11:17:17 +0200

    programa de multiplicacion rusa

---
+++ tema02/multrusa.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+int main(){
+    int a, b, suma;
+    //int b;
+    //int suma;
+    /*
+    Esto es un programa para la multiplicacion rusa
+    */  
+    printf("Dame dos números:\n");
+    scanf("%d", &a);
+    scanf("%d", &b);
+    suma = 0;
+    while (b > 1){
+        if (b is odd){
+            b--;
+            suma += a;
+        }
+        a *= 2;  // a = a * 2;
+        b /= 2;
+    }
+    printf("El resultado es %d", a + suma);
+    return 0;
+}
