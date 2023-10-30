7426e91b70ae2c2c848c7f89403c54995d8c3d03
Miguel Rebollo <mrebollo@upv.es>
2023-10-30 11:01:13 +0100

    código completo para la baraja

--- tema04/baraja.c
+++ tema04/baraja.c
@@ -8,6 +8,8 @@ int main(){
             switch(palo){
                 case 0: printf("oros\n"); break;
                 case 1: printf("copas\n"); break;
+                case 2: printf("espadas\n"); break;
+                case 3: printf("bastos\n"); break;
             } 
         } 
     } 
